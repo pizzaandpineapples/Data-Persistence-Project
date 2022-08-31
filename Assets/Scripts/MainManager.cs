8dc1120f581e85583f7bc164dd64d17e5d0ad23b
Rasheeq Kamal <41065855+pizzaandpineapples@users.noreply.github.com>
2022-08-31 12:12:39 +0600

    Update MainManager.cs
    Create a static instance of MainManager.
    
    Stop MainManager from being destroyed on load between scenes.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -6,6 +6,8 @@ using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
+    public static MainManager Instance;
+
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
@@ -18,7 +20,12 @@ public class MainManager : MonoBehaviour
     
     private bool m_GameOver = false;
 
-    
+    private void Awake()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
